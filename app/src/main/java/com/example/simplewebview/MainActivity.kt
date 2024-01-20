6e97b6f87bca46e69ea3f9b1ccd3650728d8703d
Ethan <edenvir99@gmail.com>
2024-01-20 18:16:08 +0900

    fix: make val final

--- app/src/main/java/com/example/simplewebview/MainActivity.kt
+++ app/src/main/java/com/example/simplewebview/MainActivity.kt
@@ -9,14 +9,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.simplewebview.ui.theme.SimpleWebViewTheme
 
-val PAGE_URL = "https://ethan-dev.com"
+const val PAGE_URL = "https://ethan-dev.com"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

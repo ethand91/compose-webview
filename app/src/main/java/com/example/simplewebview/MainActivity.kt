7ecb0365c4242b4599d7a86d2fd4b741a795b5d1
Ethan <edenvir99@gmail.com>
2024-01-20 15:30:00 +0900

    init

---
+++ app/src/main/java/com/example/simplewebview/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.simplewebview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.simplewebview.ui.theme.SimpleWebViewTheme
+
+val PAGE_URL = "https://ethan-dev.com"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleWebViewTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    WebViewScreen()
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewScreen() {
+    AndroidView(
+        factory = { context ->
+            return@AndroidView WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+
+                settings.loadWithOverviewMode = true
+                settings.useWideViewPort = true
+                settings.setSupportZoom(false)
+            }
+        },
+        update = {
+            it.loadUrl(PAGE_URL)
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WebViewScreenPreview() {
+    SimpleWebViewTheme {
+        WebViewScreen()
+    }
+}
